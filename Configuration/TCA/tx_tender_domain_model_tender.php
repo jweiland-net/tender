17f93e85aa610d8721942de5dcc5a6124f50fefb
kaedsei <dev@ma-ku.eu>
2018-01-31 16:47:25 +0100

    Updated acces label

--- Configuration/TCA/tx_tender_domain_model_tender.php
+++ Configuration/TCA/tx_tender_domain_model_tender.php
@@ -27,7 +27,7 @@ return [
         'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, headline, url, mediafiles, description, tenderdepartment'
     ],
     'types' => [
-        '1' => ['showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, headline, url, mediafiles, description, tenderdepartment,--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access,starttime, endtime']
+        '1' => ['showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, headline, url, mediafiles, description, tenderdepartment,--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,starttime, endtime']
     ],
     'palettes' => [
         '1' => ['showitem' => '']
