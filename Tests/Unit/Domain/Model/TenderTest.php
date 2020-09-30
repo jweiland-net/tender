4accd97d5b6dac51c56d6bb7764cd1f523ceafce
Stefan Froemken <froemken@gmail.com>
2020-09-30 16:39:26 +0200

    Remove empty line of doc header

--- Tests/Unit/Domain/Model/TenderTest.php
+++ Tests/Unit/Domain/Model/TenderTest.php
@@ -2,7 +2,6 @@
 
 /*
  * This file is part of the package jweiland/tender.
- *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
