0264cb33e6cfa41933fd1f7f2e62eaa3cf813365
Stefan Froemken <froemken@gmail.com>
2020-09-30 15:46:21 +0200

    Add Categories to model and add Tests

--- Classes/Domain/Model/Tender.php
+++ Classes/Domain/Model/Tender.php
@@ -10,6 +10,8 @@ declare(strict_types=1);
 
 namespace JWeiland\Tender\Domain\Model;
 
+use TYPO3\CMS\Extbase\Domain\Model\Category;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
@@ -39,6 +41,11 @@ class Tender extends AbstractEntity
     protected $category;
 
     /**
+     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category>
+     */
+    protected $categories;
+
+    /**
      * @var \DateTime
      */
     protected $starttime;
@@ -58,149 +65,139 @@ class Tender extends AbstractEntity
      */
     protected $description = '';
 
-    /**
-     * @return string $headline
-     */
+    public function __construct()
+    {
+        $this->categories = new ObjectStorage();
+        $this->mediafiles = new ObjectStorage();
+    }
+
     public function getHeadline(): string
     {
         return $this->headline;
     }
 
-    /**
-     * @param string $headline
-     * @return Tender
-     */
     public function setHeadline(string $headline): Tender
     {
         $this->headline = $headline;
         return $this;
     }
 
-    /**
-     * @return string $url
-     */
     public function getUrl(): string
     {
         return $this->url;
     }
 
-    /**
-     * @param string $url
-     * @return Tender
-     */
     public function setUrl(string $url): Tender
     {
         $this->url = $url;
         return $this;
     }
 
-    /**
-     * @return TenderDepartment $tenderdepartment
-     */
-    public function getTenderdepartment()
+    public function getTenderdepartment(): ?TenderDepartment
     {
         return $this->tenderdepartment;
     }
 
-    /**
-     * @param TenderDepartment $tenderdepartment
-     * @return Tender
-     */
-    public function setTenderdepartment(TenderDepartment $tenderdepartment): Tender
+    public function setTenderdepartment(?TenderDepartment $tenderdepartment = null): Tender
     {
         $this->tenderdepartment = $tenderdepartment;
         return $this;
     }
 
     /**
-     * @return ObjectStorage $category
+     * @return ObjectStorage
+     * @deprecated
      */
-    public function getCategory()
+    public function getCategory(): ObjectStorage
     {
+        trigger_error('getCategory() will be removed in tender 3.0.0. Please use getCategories() instead.', E_USER_DEPRECATED);
         return $this->category;
     }
 
     /**
      * @param ObjectStorage $category
-     * @return Tender
+     * @return $this
+     * @deprecated
      */
     public function setCategory(ObjectStorage $category): Tender
     {
+        trigger_error('setCategory() will be removed in tender 3.0.0. Please use setCategories() instead.', E_USER_DEPRECATED);
         $this->category = $category;
         return $this;
     }
 
-    /**
-     * @return \DateTime $starttime
-     */
-    public function getStarttime()
+    public function getCategories(): ObjectStorage
+    {
+        return $this->categories;
+    }
+
+    public function setCategories(ObjectStorage $categories): Tender
+    {
+        $this->categories = $categories;
+        return $this;
+    }
+
+    public function addCategory(Category $category)
+    {
+        $this->categories->attach($category);
+    }
+
+    public function removeCategory(Category $category)
+    {
+        $this->categories->detach($category);
+    }
+
+    public function getStarttime(): ?\DateTime
     {
         return $this->starttime;
     }
 
-    /**
-     * @param \DateTime $starttime
-     * @return Tender
-     */
-    public function setStarttime(\DateTime $starttime): Tender
+    public function setStarttime(?\DateTime $starttime): Tender
     {
         $this->starttime = $starttime;
         return $this;
     }
 
-    /**
-     * @return \DateTime $endtime
-     */
-    public function getEndtime()
+    public function getEndtime(): ?\DateTime
     {
         return $this->endtime;
     }
 
-    /**
-     * @param \DateTime $endtime
-     * @return Tender
-     */
-    public function setEndtime(\DateTime $endtime): Tender
+    public function setEndtime(?\DateTime $endtime): Tender
     {
         $this->endtime = $endtime;
         return $this;
     }
 
-    /**
-     * @return string $description
-     */
     public function getDescription(): string
     {
         return $this->description;
     }
 
-    /**
-     * @param string $description
-     * @return Tender
-     */
     public function setDescription(string $description): Tender
     {
         $this->description = $description;
         return $this;
     }
 
-    /**
-     * Gets the mediafiles
-     *
-     * @return ObjectStorage
-     */
     public function getMediafiles(): ObjectStorage
     {
         return $this->mediafiles;
     }
 
-    /**
-     * @param ObjectStorage $mediafiles
-     * @return Tender
-     */
     public function setMediafiles(ObjectStorage $mediafiles): Tender
     {
         $this->mediafiles = $mediafiles;
         return $this;
+    }
+
+    public function addMediafile(FileReference $fileReference)
+    {
+        $this->mediafiles->attach($fileReference);
+    }
+
+    public function removeMediafile(FileReference $fileReference)
+    {
+        $this->mediafiles->detach($fileReference);
     }
 }
---
+++ Tests/Unit/Domain/Model/TenderTest.php
@@ -0,0 +1,364 @@
+<?php
+
+/*
+ * This file is part of the package jweiland/tender.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Tender\Tests\Unit\Domain\Model;
+
+use JWeiland\Tender\Domain\Model\Tender;
+use JWeiland\Tender\Domain\Model\TenderDepartment;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use TYPO3\CMS\Extbase\Domain\Model\Category;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
+/**
+ * Test case.
+ */
+class TenderTest extends UnitTestCase
+{
+    /**
+     * @var Tender
+     */
+    protected $subject;
+
+    public function setUp()
+    {
+        $this->subject = new Tender();
+    }
+
+    public function tearDown()
+    {
+        unset($this->subject);
+    }
+
+    /**
+     * @test
+     */
+    public function getHeadlineInitiallyReturnsEmptyString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getHeadline()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setHeadlineSetsHeadline()
+    {
+        $this->subject->setHeadline('foo bar');
+
+        self::assertSame(
+            'foo bar',
+            $this->subject->getHeadline()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setHeadlineWithIntegerResultsInString()
+    {
+        $this->subject->setHeadline(123);
+        self::assertSame('123', $this->subject->getHeadline());
+    }
+
+    /**
+     * @test
+     */
+    public function setHeadlineWithBooleanResultsInString()
+    {
+        $this->subject->setHeadline(true);
+        self::assertSame('1', $this->subject->getHeadline());
+    }
+
+    /**
+     * @test
+     */
+    public function getUrlInitiallyReturnsEmptyString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getUrl()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setUrlSetsUrl()
+    {
+        $this->subject->setUrl('foo bar');
+
+        self::assertSame(
+            'foo bar',
+            $this->subject->getUrl()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setUrlWithIntegerResultsInString()
+    {
+        $this->subject->setUrl(123);
+        self::assertSame('123', $this->subject->getUrl());
+    }
+
+    /**
+     * @test
+     */
+    public function setUrlWithBooleanResultsInString()
+    {
+        $this->subject->setUrl(true);
+        self::assertSame('1', $this->subject->getUrl());
+    }
+
+    /**
+     * @test
+     */
+    public function getTenderdepartmentInitiallyReturnsNull()
+    {
+        self::assertNull($this->subject->getTenderdepartment());
+    }
+
+    /**
+     * @test
+     */
+    public function setTenderdepartmentSetsTenderdepartment()
+    {
+        $instance = new TenderDepartment();
+        $this->subject->setTenderdepartment($instance);
+
+        self::assertSame(
+            $instance,
+            $this->subject->getTenderdepartment()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getCategoriesInitiallyReturnsObjectStorage()
+    {
+        self::assertEquals(
+            new ObjectStorage(),
+            $this->subject->getCategories()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setCategoriesSetsCategories()
+    {
+        $object = new Category();
+        $objectStorage = new ObjectStorage();
+        $objectStorage->attach($object);
+        $this->subject->setCategories($objectStorage);
+
+        self::assertSame(
+            $objectStorage,
+            $this->subject->getCategories()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addCategoryAddsOneCategory()
+    {
+        $objectStorage = new ObjectStorage();
+        $this->subject->setCategories($objectStorage);
+
+        $object = new Category();
+        $this->subject->addCategory($object);
+
+        $objectStorage->attach($object);
+
+        self::assertSame(
+            $objectStorage,
+            $this->subject->getCategories()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function removeCategoryRemovesOneCategory()
+    {
+        $object = new Category();
+        $objectStorage = new ObjectStorage();
+        $objectStorage->attach($object);
+        $this->subject->setCategories($objectStorage);
+
+        $this->subject->removeCategory($object);
+        $objectStorage->detach($object);
+
+        self::assertSame(
+            $objectStorage,
+            $this->subject->getCategories()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getStarttimeInitiallyReturnsNull()
+    {
+        self::assertNull(
+            $this->subject->getStarttime()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setStarttimeSetsStarttime()
+    {
+        $date = new \DateTime();
+        $this->subject->setStarttime($date);
+
+        self::assertSame(
+            $date,
+            $this->subject->getStarttime()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getEndtimeInitiallyReturnsNull()
+    {
+        self::assertNull(
+            $this->subject->getEndtime()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setEndtimeSetsEndtime()
+    {
+        $date = new \DateTime();
+        $this->subject->setEndtime($date);
+
+        self::assertSame(
+            $date,
+            $this->subject->getEndtime()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getMediafilesInitiallyReturnsObjectStorage()
+    {
+        self::assertEquals(
+            new ObjectStorage(),
+            $this->subject->getMediafiles()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setMediafilesSetsMediafiles()
+    {
+        $object = new FileReference();
+        $objectStorage = new ObjectStorage();
+        $objectStorage->attach($object);
+        $this->subject->setMediafiles($objectStorage);
+
+        self::assertSame(
+            $objectStorage,
+            $this->subject->getMediafiles()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addMediafileAddsOneMediafile()
+    {
+        $objectStorage = new ObjectStorage();
+        $this->subject->setMediafiles($objectStorage);
+
+        $object = new FileReference();
+        $this->subject->addMediafile($object);
+
+        $objectStorage->attach($object);
+
+        self::assertSame(
+            $objectStorage,
+            $this->subject->getMediafiles()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function removeMediafileRemovesOneMediafile()
+    {
+        $object = new FileReference();
+        $objectStorage = new ObjectStorage();
+        $objectStorage->attach($object);
+        $this->subject->setMediafiles($objectStorage);
+
+        $this->subject->removeMediafile($object);
+        $objectStorage->detach($object);
+
+        self::assertSame(
+            $objectStorage,
+            $this->subject->getMediafiles()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getDescriptionInitiallyReturnsEmptyString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getDescription()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setDescriptionSetsDescription()
+    {
+        $this->subject->setDescription('foo bar');
+
+        self::assertSame(
+            'foo bar',
+            $this->subject->getDescription()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setDescriptionWithIntegerResultsInString()
+    {
+        $this->subject->setDescription(123);
+        self::assertSame('123', $this->subject->getDescription());
+    }
+
+    /**
+     * @test
+     */
+    public function setDescriptionWithBooleanResultsInString()
+    {
+        $this->subject->setDescription(true);
+        self::assertSame('1', $this->subject->getDescription());
+    }
+}
